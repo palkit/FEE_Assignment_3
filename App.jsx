7d53b235099e558bef269c27c6961ad2e02ebb9e
Palkit <122964312+palkit@users.noreply.github.com>
2024-04-19 11:37:41 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Header  from './Header'
+import Notes from './Notes'
+
+function App() {
+  
+
+  return (
+    <div className="main">
+    <Header/>
+    <Notes />
+
+  </div>
+      
+
+  )
+}
+
+export default App
